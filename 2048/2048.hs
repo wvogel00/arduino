7d18820f46641069c47230bb340c92512bd1380a
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2014-05-13 11:42:36 +0900

    fix the type of Field

--- 2048/2048.hs
+++ 2048/2048.hs
@@ -2,14 +2,13 @@ import System.Time
 import Control.Applicative
 
 data State = Clear Int | GameOver Int | OnPlay Int
-data Field = Field [[Int]]
+type Field = [[Int]]
 
-instance Show Field where
-	show (Field xs) = concat (map show' xs) ++ horizon where
-		show' xs = horizon ++ vert ++ concat (map f xs) ++ "|\n" ++ vert
-		horizon = replicate 20 '-' ++ "\n"
-		vert = concat (replicate 4 "|    ") ++ "|\n"
-		f x = "|" ++ replicate (4-length (show x)) ' ' ++ show x
+showField (Field xs) = concat (map show' xs) ++ horizon where
+	show' xs = horizon ++ vert ++ concat (map f xs) ++ "|\n" ++ vert
+	horizon = replicate 20 '-' ++ "\n"
+	vert = concat (replicate 4 "|    ") ++ "|\n"
+	f x = "|" ++ replicate (4-length (show x)) ' ' ++ show x
 
 instance Show State where
 	show state =header ++ "\n" ++ "SCORE : " ++ show (getScore state) where
@@ -22,7 +21,7 @@ getScore (Clear score) = score
 getScore (OnPlay score) = score
 getScore (GameOver score) = score
 
-initialize a b = Field $ divide 4 $ replicate i 0 ++ [2] ++ replicate (j-i-1) 0 ++ [2] ++ replicate (16-max i j - 1) 0 where
+initialize a b = divide 4 $ replicate i 0 ++ [2] ++ replicate (j-i-1) 0 ++ [2] ++ replicate (16-max i j - 1) 0 where
 	i1 = mod a 16 `min` mod b 16
 	i2 = mod a 16 `max` mod b 16
 	(i,j) = if i1 == i2 then (i1,i2+1) else (i1,i2)
@@ -37,25 +36,25 @@ main = do
 	writeFile "score.txt" $ show (getScore result)
 
 game :: Field -> State -> IO State
-game field (OnPlay score) = do
-	putStrLn $ show field
+game fd (OnPlay score) = do
+	putStrLn $ showField fd
 	command <- getChar
-	let (field',state') = next command field score
-	game field' state'
-game filed result = return result
+	let (field,state') = next command fd score
+	game field state'
+game _ result = return result
 
 next :: Char -> Field -> Int -> (Field,State)
-next key (Field fd) score = if any (==2048) $ concat fd
-	then (Field fd,Clear score)
+next key fd score = if any (==2048) $ concat fd
+	then (fd,Clear score)
 	else if cannotMove fd
-		then (Field fd,GameOver score)
+		then (fd,GameOver score)
 		else undefined
 
-cannotMove :: [[Int]] -> Bool
+cannotMove :: Field -> Bool
 cannotMove fd = not $ foldl (check fd) False [0..15]
 
 --i:縦, j:横
-check :: [[Int]] -> Bool -> Int -> Bool
+check :: Field -> Bool -> Int -> Bool
 check fd result a = result || any check' poses  where
 	(i,j) = toIndex a
 	poses = filter inField [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]
